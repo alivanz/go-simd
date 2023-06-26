07d0af7bbbd17694ae7aaf20f86f2ace191d32c6
alivanz <alivan1627@gmail.com>
2023-06-26 16:57:41 +0700

    generate neon

--- example/neon/main.go
+++ example/neon/main.go
@@ -8,12 +8,15 @@ import (
 
 func main() {
 	var a, b neon.Int8X8
+	var add, mul neon.Int16X8
 	for i := 0; i < 8; i++ {
 		a[i] = neon.Int8(i)
 		b[i] = neon.Int8(i * i)
 	}
 	log.Printf("a = %+v", b)
 	log.Printf("b = %+v", a)
-	log.Printf("add = %+v", neon.VaddlS8(a, b))
-	log.Printf("mul = %+v", neon.VmullS8(a, b))
+	neon.VaddlS8(&add, &a, &b)
+	neon.VmullS8(&mul, &a, &b)
+	log.Printf("add = %+v", add)
+	log.Printf("mul = %+v", mul)
 }
